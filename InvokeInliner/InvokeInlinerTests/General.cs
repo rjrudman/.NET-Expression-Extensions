9f589f4e95cc90638b147f0b750fc5027941645e
Robert Rudman <rjrudman@gmail.com>
2016-11-19 14:36:23 +1100

    Add two more tests

--- InvokeInliner/InvokeInlinerTests/General.cs
+++ InvokeInliner/InvokeInlinerTests/General.cs
@@ -16,10 +16,89 @@ namespace Tests
 		// ReSharper enable UnusedParameter.Local
 		{
 			var comparer = ExpressionEqualityComparer.Instance;
-			Assert.True(comparer.Equals(expected, actual), "Expressions are not equal");
+			Assert.True(comparer.Equals(expected, actual), $"Expressions are not equal. Expected: {expected}, actual: {actual}");
 	    }
 
-	    [Test]
+		[Test]
+		public void TestSimple()
+		{
+			var iParam = Expression.Parameter(typeof(int), "i");
+
+			// Invoke(i => (i + 1), 3)
+			var input = Expression.Invoke(Expression.Lambda(Expression.Add(iParam, Expression.Constant(1)), iParam), Expression.Constant(3));
+
+			// (3 + 1)
+			var expected = Expression.Add(Expression.Constant(3), Expression.Constant(1));
+
+			var actual = input.InlineInvokes();
+			AssertExpressionsEquals(expected, actual);
+		}
+
+		[Test]
+		public void TestMultiple()
+		{
+			var i = Expression.Parameter(typeof(int), "i");
+			
+			//Invoke(ia => (ia + 1), i)
+			var ia = Expression.Parameter(typeof(int), "ia");
+			var firstInvoke = Expression.Invoke(
+				Expression.Lambda(
+					Expression.Add(
+						ia,
+						Expression.Constant(1)
+					),
+					ia
+				),
+				i
+			);
+			
+			//Invoke(ib => (ib + 2), i)
+			var ib = Expression.Parameter(typeof(int), "ib");
+			var secondInvoke = Expression.Invoke(
+				Expression.Lambda(
+					Expression.Add(
+						ib,
+						Expression.Constant(2)
+					),
+					ib
+				),
+				i
+			);
+
+			var lExpr = Expression.Parameter(typeof(int), "lExpr");
+			var rExpr = Expression.Parameter(typeof(int), "rExpr");
+			// i => Invoke((lExpr, rExpr) => (lExpr * rExpr), Invoke(ia => (ia + 1), i), Invoke(ib => (ib + 2), i))
+			var input =
+				Expression.Lambda(
+					Expression.Invoke(
+						Expression.Lambda(
+							Expression.Multiply(lExpr, rExpr),
+							lExpr, rExpr
+						),
+						firstInvoke,
+						secondInvoke
+					),
+					i);
+
+			// i => ((i + 1) * (i + 2))
+			var expected = Expression.Lambda(
+				Expression.Multiply(
+					Expression.Add(
+						i,
+						Expression.Constant(1)
+					),
+					Expression.Add(
+						i,
+						Expression.Constant(2)
+					)
+				), i);
+
+			var actual = input.InlineInvokes();
+
+			AssertExpressionsEquals(expected, actual);
+		}
+
+		[Test]
 	    public void TestConstantFunction()
 	    {
 			var expected = Expression.Invoke(Expression.Constant((Func<int>)TestFunction));
